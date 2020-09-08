744ea4e03eeae74e69e8d73f8eac895107ece854
zouxiliang <zouxiliang@dstcar.com>
2020-09-08 16:36:04 +0800

    添加代码生成器

---
+++ boot-user-service/src/main/java/com/lockie/bootuser/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lockie.bootuser.mapper;
+
+import com.lockie.bootuser.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
